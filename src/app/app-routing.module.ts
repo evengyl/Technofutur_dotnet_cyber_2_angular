2427a2783ed34c36d42de1cbea66726436c88227
loic <loic.baudoux@bstorm.be>
2023-11-07 10:07:42 +0100

    add 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { Bindings1Component } from './components/bindings1/bindings1.component';
 import { Exos01Component } from './components/exos/exos01/exos01.component';
+import { FourOfourComponent } from './components/four-ofour/four-ofour.component';
 
 const routes: Routes = [
   { path : "", component : HomeComponent},
   { path : "bindings1", component : Bindings1Component},
-  { path : "exos01", component : Exos01Component}
+  { path : "exos01", component : Exos01Component},
+
+
+
+
+
+
+
+  
+  { path : "**", component : FourOfourComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { Bindings1Component } from './components/bindings1/bindings1.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { Exos01Component } from './components/exos/exos01/exos01.component';
+import { FourOfourComponent } from './components/four-ofour/four-ofour.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { Exos01Component } from './components/exos/exos01/exos01.component';
     NavbarComponent,
     FooterComponent,
     Bindings1Component,
-    Exos01Component
+    Exos01Component,
+    FourOfourComponent
   ],
   imports: [
     BrowserModule,
