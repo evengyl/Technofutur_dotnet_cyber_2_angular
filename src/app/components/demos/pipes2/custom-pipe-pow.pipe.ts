2c4c86384f1a414d200e3c2f723e086b8e242940
loic <loic.baudoux@bstorm.be>
2023-11-07 12:32:34 +0100

    last pipe custom

--- src/app/components/demos/pipes2/custom-pipe-pow.pipe.ts
+++ src/app/components/demos/pipes2/custom-pipe-pow.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CustomPipePowPipe implements PipeTransform {
 
-  transform(value: number, ...args: any[]): any {
+  transform(value: any, ...args: any[]): any {
     console.log(value)
     console.log(args)
 
@@ -15,14 +15,15 @@ export class CustomPipePowPipe implements PipeTransform {
     else if(args.length == 1)
       value = Math.pow(value, args[0])
 
-      else if(args.length == 2){
-        value = Math.pow(value, args[0])
-        value = value * ((args[1] / 100) + 1)
-      }
+    else if(args.length >= 2)
+    {
+      value = Math.pow(value, args[0])
+      value = value * ((args[1] / 100) + 1)
 
-    
-    
+      if(args.length == 3)
+        value = value + " " + args[2][0] + " " + args[2][1]
+    }
+ 
     return value;
   }
-
 }
