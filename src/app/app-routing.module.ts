c8c1d8403f0d6c6710512cc8d9cd101f053c0e72
Evengyl <dark.evengyl@gmail.com>
2023-11-08 16:51:37 +0100

    add all directives + custom

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { Pipes2Component } from './components/demos/pipes2/pipes2.component';
 import { Exos05Component } from './components/exos/exos05/exos05.component';
 import { DirectivesStruct3Component } from './components/demos/directives-struct3/directives-struct3.component';
 import { Exos1Component } from './components/exos/exos1/exos1.component';
+import { Directives4Component } from './components/demos/directives4/directives4.component';
 
 const routes: Routes = [
   { path : "", component : HomeComponent},
@@ -17,6 +18,7 @@ const routes: Routes = [
   { path : "exos05", component : Exos05Component},
   { path : "directiveStruct3", component : DirectivesStruct3Component},
   { path : "exos1", component : Exos1Component},
+  { path : "directives4", component : Directives4Component},
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,9 @@ import { Exos05Component } from './components/exos/exos05/exos05.component';
 import { BtcConverterPipe } from './components/exos/exos05/btc-converter.pipe';
 import { JhmsConverterPipe } from './components/exos/exos05/jhms-converter.pipe';
 import { DirectivesStruct3Component } from './components/demos/directives-struct3/directives-struct3.component';
-import { Exos1Component } from './components/exos/exos1/exos1.component'
+import { Exos1Component } from './components/exos/exos1/exos1.component';
+import { Directives4Component } from './components/demos/directives4/directives4.component';
+import { ZoomitDirective, ZoomitEventDirective, ZoomitEventParamsDirective } from './components/demos/directives4/custom/zoomit.directive'
 
 registerLocaleData(localFr)
 
@@ -40,7 +42,11 @@ registerLocaleData(localFr)
     BtcConverterPipe,
     JhmsConverterPipe,
     DirectivesStruct3Component,
-    Exos1Component
+    Exos1Component,
+    Directives4Component,
+    ZoomitDirective,
+    ZoomitEventDirective,
+    ZoomitEventParamsDirective
   ],
   imports: [
     BrowserModule,
